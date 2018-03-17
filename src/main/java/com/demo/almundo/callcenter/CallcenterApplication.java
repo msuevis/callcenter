6bb474283fa62d836ce10ea2c7fa654fa8ff860c
miguel.suevis <miguel.suevis@payulatam.com>
2018-03-17 12:34:01 -0500

    se crea la estructura inicial del proyecto

---
+++ src/main/java/com/demo/almundo/callcenter/CallcenterApplication.java
@@ -0,0 +1,12 @@
+package com.demo.almundo.callcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallcenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CallcenterApplication.class, args);
+	}
+}
